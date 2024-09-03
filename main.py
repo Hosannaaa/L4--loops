1e29d4f1fd1b75db2103b1e5e30a01f26e45b09c
Hosannaaaa <hosannabilewu@gmail.com>
2024-09-03 12:45:34 +0100

    loops

---
+++ main.py
@@ -0,0 +1,13 @@
+# LOOPS
+for a in range(1,11,2):
+  print(a)
+
+for b in range(1,11) :
+   print(b*23)
+
+#    star pattern
+n=int(input("enter the number of rows:"))
+for c in range(n+1):
+   for d in range(c):
+      print("*",end="")
+   print()
